f8f46662f939da033d6e66605297859a37a40e06
Syed Muhammad Hamza <syedmuhammadhamza2019@gmail.com>
2024-01-03 14:37:13 +0500

    initial code

--- src/components/FbPost/index.js
+++ src/components/FbPost/index.js
@@ -2,34 +2,55 @@ import FbImageLibrary from "react-fb-image-grid";
 import CustomBtn from "../CustomBtn";
 import "./index.css";
 import globe from "./globe.png";
+import profile from "./post-image.png";
+import care from "./care.png"
+import sad from "./sad.png"
+import love from "./love.png"
 
 function FbPost({post}) {
   return (
     <div>
       <div className="fb-post-div">
         <div className="post-info">
-          <h3>
-          {post.title} - {post.category}
-          </h3>
-          <div className="sponsored-div">
-            <div>Sponsored . </div>
-            <img src={globe} width="12px" />
+          <div className="profile-div">
+            <div className="profile-img-div">
+              <img src={profile} className="profile-img" />
+            </div>
+            <div>
+              <h3 className="post-title">
+                {post.title} - {post.category}
+              </h3>
+              <div className="sponsored-div">
+                <div>Sponsored . </div>
+                <img src={globe} width="12px" />
+              </div>
+            </div>
           </div>
-          <p>{post.description}</p>
+          <p className="description">{post.description}</p>
         </div>
-      <div style={{ width: "100%" }}>
+        <div style={{ width: "100%" }}>
           <FbImageLibrary images={post.images} />
-      </div>
-      <div>
-      <div className="btn-div">
-          <CustomBtn text="Like" className="like-btn" />
-          <CustomBtn text="Comment" className="comment-btn" />
-          <CustomBtn text="Share" className="share-btn" />
+        </div>
+        <div>
+          <div className="emoji-div">
+            <div className="emoji">
+              <img src={sad} className="emoji-img"/>
+              <img src={love} className="emoji-img"/>
+              <img src={care} className="emoji-img"/>
+            </div>
+            <div className="userName">
+              <p>S. M. Hamza, Osama Sohail and 1.5K others</p>
+              <p>8 Comments</p>
+              <p>30 Shares</p>
+            </div>
+          </div>
+          <div className="btn-div">
+            <CustomBtn text="Like" className="like-btn" />
+            <CustomBtn text="Comment" className="comment-btn" />
+            <CustomBtn text="Share" className="share-btn" />
+          </div>
         </div>
       </div>
-      </div>
-
-      
     </div>
   );
 }
